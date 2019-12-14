cf7b1982f1e8198d25e35df832168a7a4f7f26f8
taty2010 <shydalys123@gmail.com>
2019-12-13 20:27:11 -0600

    added ternary operator for gender/avatar choices

--- team-builder/src/Components/TeamMember.js
+++ team-builder/src/Components/TeamMember.js
@@ -7,7 +7,9 @@ const TeamMember = props =>{
         <div className='teamList'>
             {props.person.map(person =>(
                 <div key={person.id} className='person'>
-                    <img src={person.img} alt='avatar' width='100' height='100'/>
+                    <div>{
+                    person.gender === 'female' ? <img src={person.img} alt='avatar' width='100' height='100'/>
+                    : person.gender === 'neutral' ? <img src={person.imgN} alt='avatar' width='120' height='100'/>:<img src={person.imgM} alt='avatar' width='100' height='100'/>}</div>
                     <h2>{`${person.firstName}  ${person.lastName}`}</h2>
                     <h3>{person.role}</h3>
                     <p>{person.email}</p>
